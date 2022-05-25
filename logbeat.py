96c4d8bd2c1cd97c88d72ffa53a05bdbca67d25c
gluckzhang <zhanglong3030@qq.com>
2022-05-25 16:30:07 +0200

    implement file monitor and error reporter

--- logbeat.py
+++ logbeat.py
@@ -3,6 +3,7 @@
 # Filename: logbeat.py
 
 import os, sys, argparse, configparser, re, time
+from collections import deque
 import logging
 
 __version__ = "0.1"
@@ -22,7 +23,12 @@ def get_args():
 
     return config
 
-def handle_message(message_format, timestamp_format, session_id_format, message_body_format, message):
+def message_handler(logfile_format, message):
+    message_format = logfile_format["message_format"]
+    timestamp_format = logfile_format["timestamp_format"]
+    session_id_format = logfile_format["session_id_format"]
+    message_body_format = logfile_format["message_body_format"]
+
     message_pattern = message_format
     message_pattern = message_pattern.replace("TIMESTAMP", timestamp_format)
     message_pattern = message_pattern.replace("SESSION_ID", session_id_format)
@@ -30,6 +36,11 @@ def handle_message(message_format, timestamp_format, session_id_format, message_
 
     message_pattern_re = re.compile(message_pattern)
     match = message_pattern_re.search(message)
+
+    if match == None:
+        logging.error("Incorrectly formated message detected.")
+        return None
+
     session_id = int(match.group("session_id"))
     message_body = match.group("message_body")
 
@@ -37,17 +48,36 @@ def handle_message(message_format, timestamp_format, session_id_format, message_
 
     return session_id, if_error, message
 
-def main(config):
-    message_format = config["LogFormat"]["message_format"]
-    timestamp_format = config["LogFormat"]["timestamp_format"]
-    session_id_format = config["LogFormat"]["session_id_format"]
-    message_body_format = config["LogFormat"]["message_body_format"]
-    number_of_messages_included = int(config["LogBeat"]["number_of_messages_included"])
+def generate_error_report(message_queue):
+    error_report = ""
+    delimiter = "-----"
+    while len(message_queue) > 0:
+        error_report += message_queue.pop()
+    error_report += delimiter
+    print(error_report)
 
-    test_message = "2022-05-25 14:23:30 [120] ERROR: This is an error message!"
-    logging.info(handle_message(message_format, timestamp_format, session_id_format, message_body_format, test_message))
-    test_message = "2022-05-25 14:23:30 [120] This is a normal message!"
-    logging.info(handle_message(message_format, timestamp_format, session_id_format, message_body_format, test_message))
+def file_monitor(filepath, logfile_format, handler, frequency, number_of_messages_included):
+    deques_for_sids = dict()
+    with open(filepath) as logfile:
+        while True:
+            message = logfile.readline()
+            if not message:
+                time.sleep(frequency)
+            else:
+                sid, if_error, message = handler(logfile_format, message)
+                if sid not in deques_for_sids:
+                    deques_for_sids[sid] = deque(maxlen=number_of_messages_included)
+
+                deques_for_sids[sid].appendleft(message)
+                if if_error: generate_error_report(deques_for_sids[sid])
+
+def main(config):
+    number_of_messages_included = int(config["LogBeat"]["number_of_messages_included"])
+    seconds_between_checks = float(config["LogBeat"]["seconds_between_checks"])
+    logfile_path = config["LogBeat"]["logfile_path"]
+    logfile_format = config["LogFormat"]
+
+    file_monitor(logfile_path, logfile_format, message_handler, seconds_between_checks, number_of_messages_included)
 
 
 if __name__ == "__main__":

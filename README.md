c1ebcac3eb2e354f8575980b60f7929e5bd845cb
gluckzhang <zhanglong3030@qq.com>
2022-05-26 13:58:50 +0200

    doc: add some comments for the code

--- README.md
+++ README.md
@@ -18,18 +18,31 @@ Requirements for the error report:
 
 Besides the requirements above, the implementation of LogBeat should be well-tested using proper tools.
 
-Additional thoughts of the tool:
+## Additional thoughts of the tool
 
 - It should be configurable, like the parth to the target folder or file, the format of logs, etc.
+
+This has been implemented. In the configuration file, one can either specify the path to a log file, or the path to a folder that contains log files. If a folder is specified, LogBeat keeps monitoring the files in that foler and adds newly created log files to the monitoring list as well.
+
 - Since it is a **monitor** instead of an **analyzer**, performance matters. What if a log file is huge, with a large number of session IDs?
+
+I have not evaluated the performance of LogBeat yet. However, if many lines are written into a log file in a short period, or there are many different session IDs in these log files, it might cause some overhead with respect to CPU usage and memory usage. Because in the current implementation, each session has a fixed-length deque for storing its log messages.
+
+There can be at least two directions to improve the performance. First of all, the architecture and data-structures of LogBeat can be further improved. Second, it is not necessary to run such log error reporters in the same instance where the application is running. Usually we can ship the oroginal logs to a centralized database like ElasticSearch or Google Cloud's logs explorer. Then further analysis of the logs such as error detection and reporting can be done asynchronously.
+
 - CI/CD could be applied using GitHub Actions.
+
+Sure. Currently there are two GitHub actions: unit-testing, and reporting the coverage information to [coveralls](https://coveralls.io/).
+
 - Dockerized it?
+
+Considering that LogBeat does not have lots of dependencies to install, I have not made a Dockerfile for it for now. As long as Python 3 is installed, one can easily run LogBeat.
 
 ## What do I need to implement
 
 - [X] Code skeleton
 - [X] Read configurations from a file
 - [X] Sub-threads management (make sure to kill all the sub-threads when LogBeat stops)
-- [ ] Unit testing
+- [ ] Unit testing and integration testing
 - [X] CI using GitHub Action
-- [ ] Documentation
+- [X] Documentation and code comments
--- logbeat.py
+++ logbeat.py
@@ -11,6 +11,7 @@ __version__ = "0.1"
 MONITOR_THREADS = list()
 
 def handle_sigint(sig, frame):
+    """When LogBeat exits, all the file monitors should be terminated."""
     global MONITOR_THREADS
     for thread in MONITOR_THREADS:
         if thread.is_alive():
@@ -19,6 +20,7 @@ def handle_sigint(sig, frame):
     exit()
 
 def get_args():
+    """Handle command line arguments and return if some required configs are missing"""
     parser = argparse.ArgumentParser(
         description="LogBeat, report the errors in your log files")
     parser.add_argument("-c", "--config", required=True,
@@ -39,11 +41,23 @@ def get_args():
     return config
 
 def message_handler(logfile_format, message):
-    message_format = logfile_format["message_format"]
-    timestamp_format = logfile_format["timestamp_format"]
-    session_id_format = logfile_format["session_id_format"]
-    message_body_format = logfile_format["message_body_format"]
+    """When there is a new line of message in a monitored log file, this method extracts the information of the message
 
+    arguments:
+    logfile_format: describes what format this message follows, read from the configuration file
+    message: the string type message read from the log file
+
+    return:
+    session_id: integer type of a session_id
+    if_error: True if the message body starts with ERROR
+    message: string of the original message
+    """
+    message_format = logfile_format["message_format"] # e.g. TIMESTAMP SESSION_ID MESSAGE_BODY
+    timestamp_format = logfile_format["timestamp_format"] # e.g. (?P<timestamp>\d+-\d+-\d+ \d+:\d+:\d+)
+    session_id_format = logfile_format["session_id_format"] # e.g. \[(?P<session_id>\d+)\]
+    message_body_format = logfile_format["message_body_format"] # e.g. (?P<message_body>[\w\W]+$)
+
+    # construct the regular expression for the whole message
     message_pattern = message_format
     message_pattern = message_pattern.replace("TIMESTAMP", timestamp_format)
     message_pattern = message_pattern.replace("SESSION_ID", session_id_format)
@@ -52,10 +66,12 @@ def message_handler(logfile_format, message):
     message_pattern_re = re.compile(message_pattern)
     match = message_pattern_re.search(message)
 
+    # if the message does not follow the format
     if match == None:
         logging.error("Incorrectly formated message detected: %s"%message.encode("utf-8"))
         return (None, None, message)
 
+    # extract session_id and message_body from the named groups
     session_id = int(match.group("session_id"))
     message_body = match.group("message_body")
 
@@ -64,6 +80,15 @@ def message_handler(logfile_format, message):
     return session_id, if_error, message
 
 def generate_error_report(filepath, message_queue):
+    """When an error message is detected, generate an error port for that specific session
+
+    arguments:
+    filepath: the path to the monitored log file (this is used when there are multiple log files being monitored)
+    message_queue: a fixed-length queue that contains at most N (pre-configured) recent messages
+
+    return:
+    error_report: string of the whole error report, with a delimiter as the last line
+    """
     global MONITOR_THREADS
 
     if len(MONITOR_THREADS) > 0:
@@ -80,6 +105,18 @@ def generate_error_report(filepath, message_queue):
 
 def file_monitor(filepath, logfile_format, handler, frequency,
             number_of_messages_included, handle_existing_lines):
+    """This method keeps monitoring a given log file, tails its new lines of messages and passes them to a message handler
+
+    arguments:
+    filepath: the path to a log file to be monitored
+    logfile_format: describes what format this message follows, read from the configuration file
+    handler: a method that handles log messages
+    frequency: the monitor regularly checks the logfile, frequency defines how many seconds between each check
+    number_of_messages_included: in an error report, at most this number of messages are included
+    handle_existing_lines: 0 or 1, 0 means the monitor only handles new lines of messages after it is started, 1 means the monitor also analyzes the existing lines of logs
+    """
+
+    # Every session_id's log messages are stored in a separate fixed-length queue
     deques_for_sids = dict()
     with open(filepath) as logfile:
         if not handle_existing_lines: logfile.seek(0, 2)
@@ -94,9 +131,10 @@ def file_monitor(filepath, logfile_format, handler, frequency,
                         deques_for_sids[sid] = deque(maxlen=number_of_messages_included)
 
                     deques_for_sids[sid].appendleft(message)
-                    if if_error: print(generate_error_report(filepath, deques_for_sids[sid]))
+                    if if_error: print(generate_error_report(filepath, deques_for_sids[sid])) # generate an error report because an error message is detected
 
 def main(config):
+    """Entry of LogBeat"""
     global MONITOR_THREADS
 
     number_of_messages_included = int(config["LogBeat"]["number_of_messages_included"])
@@ -107,6 +145,7 @@ def main(config):
     logfile_format = config["LogFormat"]
 
     if logfile_path != "":
+        # If logfile_path is specified, LogBeat only monitors this file
         file_monitor(logfile_path,
             logfile_format,
             message_handler,
@@ -114,6 +153,7 @@ def main(config):
             number_of_messages_included,
             handle_existing_lines)
     elif logfile_folder != "":
+        # If logfile_folder is specified, LogBeat uses sub-threads to monitor all the .log files in that folder
         monitored_files = list()
         while True:
             for entry in os.listdir(logfile_folder):
